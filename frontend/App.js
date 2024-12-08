a6eee640bd571ed758070542ee23bdbe2f5ce3ae
Kavintha Dinushan <kavinthad03@gmail.com>
2024-12-08 21:46:27 +0530

    frontend update
    added gem lot registration screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Gem_lot_register from './src/screens/Gem_lot_register';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Gem_lot_register />
     </View>
   );
 }
@@ -13,8 +13,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });

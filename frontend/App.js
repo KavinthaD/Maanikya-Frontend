232c37a7089cb0da4a7da044e8380da8936481a6
nini20232217 <thulanikalatuwawa17@gmail.com>
2024-12-16 16:03:36 +0530

    selection page for register

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Gem_lot_register from './src/screens/Gem_lot_register';
+import SelectionPage from './src/screens/selectionPage';
 import NavigationBar from './src/components/BS_NavBar';
 import BS_NavBar from './src/components/BS_NavBar';
+
 
 export default function App() {
   return (

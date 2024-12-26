2fcca6e82ac96d48964a9911eddf38e5a191ae59
Isum Perera <160204326+isum03@users.noreply.github.com>
2024-12-26 08:06:21 +0530

    gems on display page
    gem on display page

--- frontend/App.js
+++ frontend/App.js
@@ -4,12 +4,13 @@ import RegisterSelectionPage from './src/screens/RegisterSelectionPage';
 import Gem_lot_register from './src/screens/Gem_lot_register';
 import NavigationBar from './src/components/BS_NavBar';
 import BS_NavBar from './src/components/BS_NavBar';
-
+import MyGems from './src/screens/MyGems';
+import GemOnDisplay from './src/screens/GemOnDisplay';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <BS_NavBar />
+      <GemOnDisplay />
     </View>
   );
 }

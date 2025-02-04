7bcce0ad3362d521dd8ef29a53c718176d66c1a8
Mehara Wilfred <wilfredmehara@gmail.com>
2025-02-04 20:26:16 +0530

    commit for refactoring app.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,40 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-<<<<<<< Updated upstream
+import FinancialRecords from './src/screens/FinancialRecords';
 import MySellersScreen from './src/screens/CustomerFinancialRecords';
-// import RegisterSelectionPage from './src/screens/RegisterSelectionPage';
-// import Gem_lot_register from './src/screens/Gem_lot_register';
-// import PurposeSelectionPage from './src/screens/PurposeSelectionPage';
-// import NavigationBar from './src/components/BS_NavBar';
-// import BS_NavBar from './src/components/BS_NavBar';
-// import MyGems from './src/screens/MyGems';
-// import GemOnDisplay from './src/screens/GemOnDisplay';
-=======
-import RegisterSelectionPage from './src/screens/RegisterSelectionPage';
-import Gem_lot_register from './src/screens/Gem_lot_register';
-import PurposeSelectionPage from './src/screens/PurposeSelectionPage';
-import NavigationBar from './src/components/BS_NavBar';
-import BS_NavBar from './src/components/BS_NavBar';
-import MyGems from './src/screens/MyGems';
-import GemOnDisplay from './src/screens/GemOnDisplay';
-import MySellersScreen from './src/screens/CustomerFinancialRecords';
-// import FinancialRecords from './src/screens/FinancialRecords';
-// import AlertsScreen from './src/screens/AlertsScreen';
->>>>>>> Stashed changes
-// import BusinessOwnerProfile from './src/screens/BusinessOwnerProfile';
 
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< Updated upstream
+      {/* <FinancialRecords /> */}
       <MySellersScreen/>
-      {/* <BusinessOwnerProfile /> */}
-=======
-      {/* <BusinessOwnerProfile /> */}
-      {/* <FinancialRecords/> */}
-      {/* <AlertsScreen/> */}
-      <MySellersScreen/>
->>>>>>> Stashed changes
     </View>
   );
 }

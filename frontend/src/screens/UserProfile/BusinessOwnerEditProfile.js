6048537f25db449d207f2e2100f3d44c7ebba403
Isum Perera <160204326+isum03@users.noreply.github.com>
2025-03-05 22:55:22 +0530

    updated ui of

--- frontend/src/screens/UserProfile/BusinessOwnerEditProfile.js
+++ frontend/src/screens/UserProfile/BusinessOwnerEditProfile.js
@@ -1,14 +1,16 @@
 //Screen creator: Isum
 
 import React, { useState } from "react";
-import {View, Text, TextInput,Image,TouchableOpacity,StyleSheet} from "react-native";
+import {View, Text, TextInput,Image,TouchableOpacity,StyleSheet, Modal, KeyboardAvoidingView, Platform} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons} from "@expo/vector-icons";
+import ImageCropPicker from "react-native-image-crop-picker";
 
 
-const BusinessOwnerEditProfile = ({ navigation }) => {
+const BusinessOwnerEditProfile = ({ navigation, route }) => {
+ 
   // Profile state
-  const [profilePhoto, setProfilePhoto] = useState("https://static.wikia.nocookie.net/garfield/images/6/60/Garfield_New_Look.jpg/revision/latest/scale-to-width/360?cb=20240328075614");
+  const [profilePhoto, setProfilePhoto] = useState(null);
   const [name, setName] = useState("M.D Rathnasiri Navasinghe");
   const [email, setEmail] = useState("rathnasiri.n@hotmail.com");
   const [contact, setContact] = useState("+94 987 654 321");
@@ -16,66 +18,150 @@ const BusinessOwnerEditProfile = ({ navigation }) => {
   const [address, setAddress] = useState(
     "602, Kalawana Rd, Nivitigala, Rathnapura, Sri Lanka"
   );
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const handleCameraPress = () => {
+    setModalVisible(true);
+  };
+
+  const handleTakePhoto = async () => {
+    try {
+      const result = await ImageCropPicker.openCamera({
+        width: 600,
+        height: 600,
+        mediaType: "photo",
+        includeBase64: false,
+        maxHeight: 2000,
+        maxWidth: 2000,
+        cropping: true,
+        cropperCircleOverlay: false,
+        cropperStatusBarColor: "#9CCDDB",
+        cropperToolbarColor: "#9CCDDB",
+      });
+
+      if (result && result.path) {
+        setProfilePhoto(result.path);
+      }
+    } catch (error) {
+      console.error("Error taking photo:", error);
+    } finally {
+      setModalVisible(false);
+    }
+  };
+
+  const handleChooseFromGallery = async () => {
+    try {
+      const result = await ImageCropPicker.openPicker({
+        width: 600,
+        height: 600,
+        mediaType: "photo",
+        includeBase64: false,
+        maxHeight: 2000,
+        maxWidth: 2000,
+        cropping: true,
+        cropperCircleOverlay: false,
+        cropperStatusBarColor: "#9CCDDB",
+        cropperToolbarColor: "#9CCDDB",
+      });
+
+      if (result && result.path) {
+        setProfilePhoto(result.path);
+      }
+    } catch (error) {
+      console.error("Error choosing from gallery:", error);
+    } finally {
+      setModalVisible(false);
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{ flex: 1 }}
+          keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+      >
       {/* Profile photo */}
-      <View style={styles.profileContainer}>
-        <Image
-          source={
-            profilePhoto ? { uri: profilePhoto } : require("../assets/logo.png")
-          }
-          style={styles.profilePhoto}
-        />
-        {/*edit photo button*/}
-        <TouchableOpacity style={styles.editPhotoBtn} >
-          <Ionicons name="pencil" size={20} color ="#0a3a5d" />
+        <View style={styles.profileContainer}>
+          <Image
+            source={
+              profilePhoto ? { uri: profilePhoto } : {uri:"https://upload.wikimedia.org/wikipedia/commons/a/a4/2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg"}
+            }
+            style={styles.profilePhoto}
+          />
+          {/*edit photo button*/}
+          <TouchableOpacity style={styles.editPhotoBtn} onPress={handleCameraPress}>
+            <Ionicons name="pencil" size={20} color ="#0a3a5d" />
+          </TouchableOpacity>
+        </View>
+        {/*edit fields*/}    
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>NAME</Text>
+          <TextInput
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+            placeholderTextColor="#777"
+          />
+
+          <Text style={styles.label}>EMAIL</Text>
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            placeholderTextColor="#777"
+          />
+
+          <Text style={styles.label}>Contact No</Text>
+          <TextInput
+            style={styles.input}
+            value={contact}
+            onChangeText={setContact}
+            keyboardType="phone-pad"
+            placeholderTextColor="#777"
+          />
+
+          <Text style={styles.label}>TITLE</Text>
+          <TextInput style={styles.input} value={title} onChangeText={setTitle} />
+
+          <Text style={styles.label}>Address</Text>
+          <TextInput
+            style={[styles.input, styles.textArea]}
+            value={address}
+            onChangeText={setAddress}
+            multiline
+            placeholderTextColor="#777"
+          />
+        </View>
+        {/*save button*/}    
+        <TouchableOpacity style={styles.saveBtn} onPress={() => navigation.goBack()}>
+          <Text style={styles.saveBtnText}>Save</Text>
         </TouchableOpacity>
-      </View>
-      {/*edit fields*/}    
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>NAME</Text>
-        <TextInput
-          style={styles.input}
-          value={name}
-          onChangeText={setName}
-          placeholderTextColor="#777"
-        />
 
-        <Text style={styles.label}>EMAIL</Text>
-        <TextInput
-          style={styles.input}
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          placeholderTextColor="#777"
-        />
-
-        <Text style={styles.label}>Contact No</Text>
-        <TextInput
-          style={styles.input}
-          value={contact}
-          onChangeText={setContact}
-          keyboardType="phone-pad"
-          placeholderTextColor="#777"
-        />
-
-        <Text style={styles.label}>TITLE</Text>
-        <TextInput style={styles.input} value={title} onChangeText={setTitle} />
-
-        <Text style={styles.label}>Address</Text>
-        <TextInput
-          style={[styles.input, styles.textArea]}
-          value={address}
-          onChangeText={setAddress}
-          multiline
-          placeholderTextColor="#777"
-        />
-      </View>
-      {/*save button*/}    
-      <TouchableOpacity style={styles.saveBtn} onPress={() => navigation.goBack()}>
-        <Text style={styles.saveBtnText}>Save</Text>
-      </TouchableOpacity>
+        <Modal
+          visible={isModalVisible}
+          onRequestClose={() => setModalVisible(false)}
+          transparent={true}
+          animationType="slide"
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <TouchableOpacity style={styles.modalButton} onPress={handleTakePhoto}>
+                <Ionicons name="camera" size={24} color="#170969" />
+                <Text style={styles.modalButtonText}>Take Photo</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.modalButton} onPress={handleChooseFromGallery}>
+                <Ionicons name="image" size={24} color="#170969" />
+                <Text style={styles.modalButtonText}>Choose from Gallery</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={[styles.modalButton, styles.cancelButton]} onPress={() => setModalVisible(false)}>
+                <Text style={[styles.modalButtonText, styles.cancelButtonText]}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      </KeyboardAvoidingView>  
     </SafeAreaView>
   );
 };
@@ -146,6 +232,44 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  modal: {
+    justifyContent: "flex-end",
+    margin: 0,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 22,
+    borderTopLeftRadius: 17,
+    borderTopRightRadius: 17,
+    alignItems: "center",
+  },
+  modalButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    backgroundColor: "#E8F0FE",
+    marginBottom: 10,
+    width: "100%",
+    justifyContent: "center",
+  },
+  modalButtonText: {
+    fontSize: 18,
+    color: "#170969",
+    marginLeft: 10,
+  },
+  cancelButton: {
+    backgroundColor: "#f8d7da",
+  },
+  cancelButtonText: {
+    color: "#721c24",
   },
 });
 
